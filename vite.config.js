b00711dbbcd61e75752f8613dbf474d2f2aa302f
davidmachado-o <davidmachadodj@gmail.com>
2022-08-18 16:16:55 -0300

    trying to solve blank page issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://davidmachado-o/breaking-bad/',
+  base: 'https://davidmachado-o.github.io/breaking-bad/',
   plugins: [react()]
 })
